40de7871276652ba2cb59819b507bbb13b1a36f3
al3x-s <batman69x25@gmail.com>
2022-08-06 02:36:18 -0500

    Added Js Logic

--- script.js
+++ script.js
@@ -2,12 +2,29 @@ let first_val;
 let second_val;
 let action;
 
+if(first_val === undefined){
+    console.log("riuns")
+}
+
 
 function calc(id){
-    let val = parseInt(id)
-    let conditional = isNaN(val)
-    if(conditional === false){
-        console.log(val)
-        document.getElementById("active").innerHTML = val
+    if(first_val === undefined){
+        let val = parseInt(id)
+        let conditional = isNaN(val)
+        if(conditional === false){
+            console.log(val)
+            document.getElementById("active").innerHTML = val
+            first_val = val
+        }
+    else if(first_val != undefined){
+        let temp_second_val = parseInt(id)
+        let conditional = isNaN(temp_second_val)
+        if(conditional === false){
+            console.log(temp_second_val)
+            document.getElementById("active").innerHTML = temp_second_val
+            second_val = temp_second_val
+        }
     }
+    
+}
 }
