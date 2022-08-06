1acfcdfda2b8c3041463c707d3f7b2a646755de5
al3x-s <batman69x25@gmail.com>
2022-08-06 14:29:12 -0500

    New logic changes might redo

--- script.js
+++ script.js
@@ -12,19 +12,23 @@ function calc(id){
         let val = parseInt(id)
         let conditional = isNaN(val)
         if(conditional === false){
+            console.log("first_Val")
             console.log(val)
             document.getElementById("active").innerHTML = val
             first_val = val
         }
+    }
     else if(first_val != undefined){
         let temp_second_val = parseInt(id)
         let conditional = isNaN(temp_second_val)
         if(conditional === false){
+            console.log("second_val")
             console.log(temp_second_val)
             document.getElementById("active").innerHTML = temp_second_val
             second_val = temp_second_val
         }
     }
-    
-}
+    if(second_val !== undefined){
+        document.getElementById("alert_text").innerHTML = "already chose 2 values"
+    }
 }
